ae35220b600b4ea9b640d85a788ad3b613ad1d8f
fahmed0710 <fahmed0710@gmail.com>
2026-05-11 22:19:32 -0700

    init proj

---
+++ app/letters/page.tsx
@@ -0,0 +1,38 @@
+import { promises as fs } from 'fs';
+
+export default async function LettersPage() {
+  interface Conjunct {
+    part1: string;
+    part2: string;
+    conjunct: string;
+  }
+
+  const file = await fs.readFile(process.cwd() + '/app/conjuncts.json', 'utf8');
+  const conjuncts = JSON.parse(file);
+
+  console.log(conjuncts);
+  return (
+    <div>
+      Letters
+      <table>
+        <thead>
+          <tr>
+            <th>Letter 1</th>
+            <th>Letter 2</th>
+            <th>Conjunct</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {conjuncts.map((conjunct: Conjunct, index: any) => (
+            <tr key={index}>
+              <td>{conjunct.part1}</td>
+              <td>{conjunct.part2}</td>
+              <td>{conjunct.conjunct}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
